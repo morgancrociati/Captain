182e73eceec43c38437ecb3b0ce15eb7f21073be
Claire548 <claire.velut@laposte.net>
2021-02-11 14:40:35 +0100

    Add cumulative movement

--- shared/Globals.gd
+++ shared/Globals.gd
@@ -1,8 +1,8 @@
 extends Node
 
 #constant for directions
-class Direction:
-	const NORTH = Vector2(0,-1)
-	const SOUTH = Vector2(0,1)
-	const EAST = Vector2(1,0)
-	const WEST = Vector2(-1,0)
+
+const NORTH = Vector2(0,-1)
+const SOUTH = Vector2(0,1)
+const EAST = Vector2(1,0)
+const WEST = Vector2(-1,0)
